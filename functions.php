8077dfefc0437a146c506d125b13c227ee138b4b
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-14 18:16:17 +0000

    Bug fix and register the Topics taxonomy

--- functions.php
+++ functions.php
@@ -18,7 +18,9 @@ class docredux {
 		$this->staff = new docredux_staff();
 		
 		// Add support for post formats
-		add_hook( 'after_setup_theme', array( &$this, 'add_post_formats' ) );
+		add_action( 'after_setup_theme', array( &$this, 'add_post_formats' ) );
+		
+		add_action( 'init', array( &$this, 'create_taxonomies' ) );
 		
 	} // END __construct()
 	
@@ -34,6 +36,39 @@ class docredux {
 	 * Register taxonomies for all of our post types
 	 */
 	function create_taxonomies() {
+		
+		$args = array(
+			'label' => 'Topics',
+			'labels' => array(
+				'name' => 'Topics',
+				'singular_name' => 'Topic',
+				'search_items' =>  'Search Topics',
+				'popular_items' => 'Popular Topics',
+				'all_items' => 'All Topics',
+				'parent_item' => 'Parent Topics',
+				'parent_item_colon' => 'Parent Topics:',
+				'edit_item' => 'Edit Topic', 
+				'update_item' => 'Update Topic',
+				'add_new_item' => 'Add New Topic',
+				'new_item_name' => 'New Topic',
+				'separate_items_with_commas' => 'Separate topics with commas',
+				'add_or_remove_items' => 'Add or remove topics',
+				'choose_from_most_used' => 'Choose from the most common topics',
+				'menu_name' => 'Topics',
+			),
+			'show_tagcloud' => false,
+			'rewrite' => array(
+				'slug' => 'topics',
+				'hierarchical' => true,
+			),
+		);
+		
+		$post_types = array(
+			'post',
+			'docredux_documentation',
+			'docredux_staff',
+		);
+		register_taxonomy( 'docredux_topics', $post_types, $args );
 		
 	} // END create_taxonomies()
 	
