30196e98b9003e50da7dc141ba0808d22daf72de
joe <hover2pi@gmail.com>
2011-04-29 19:10:23 +0000

    Styled the meta box on the search results.

--- search.php
+++ search.php
@@ -22,6 +22,23 @@
 							<?php if ( has_post_thumbnail()) { 	   
         					   the_post_thumbnail(  array(60,60), array('class' => 'avatar')); 
         					}?>
+        					
+        					<div class="meta">
+								<?php 
+									global $current_user;	
+									get_currentuserinfo();
+									if ($current_user->user_level == 10 ) {
+										edit_post_link('Edit this post', '', ' — ');
+									}
+		 							the_time('l, F jS, Y');
+									echo get_the_term_list( $post->ID, 'docredux_courses', ' — ', ', ', '' );
+									echo get_the_term_list( $post->ID, 'docredux_topics', ', ', ', ', '' );
+									echo get_the_term_list( $post->ID, 'docredux_hardware', ', ', ', ', '' );
+									echo get_the_term_list( $post->ID, 'docredux_software', ', ', ', ', '' );
+									echo get_the_term_list( $post->ID, 'docredux_wpthemes', ', ', ', ', '' );
+									echo get_the_term_list( $post->ID, 'docredux_wpplugins', ', ', ', ', '' );
+								?>
+							</div><!-- END .meta -->
 
 						</div><!-- END - .staff-index -->
 
