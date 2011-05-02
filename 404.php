24f603f97e7f5a1ad47f48645a6d8ac416434ee5
joe <hover2pi@gmail.com>
2011-05-02 16:43:17 +0000

    Tweaked CSS for the fail whale. 404 page is done.

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@
                 <h2>Oops... 404!</h2>
                 <p>You've stumbled upon the infamous CUNY J-School fail whale. Try browsing or do a search to continue on your way!</p>
                 <div class="fail-whale">
-                    <img src="<?php bloginfo('template_directory'); ?>/images/fail-whale" alt="Fail Whale" />
+                    <img src="<?php bloginfo('template_directory'); ?>/images/fail-whale.png" alt="Fail Whale" />
                 </div>
 			
 			</div><!-- END .archive -->
