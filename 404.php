032f6621f639cf3bb08d250a54a4f8edbc835b45
joe <hover2pi@gmail.com>
2011-05-02 16:28:05 +0000

    Added a 404 page with a fail whale graphic. Added images for the gradients in browsers without CSS3.

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<div class="main">
+	
+	<div class="container">
+		
+		<div class="content left w600">
+			
+			<div class="pads">			
+
+                <h2>Oops... 404!</h2>
+                <p>You've stumbled upon the infamous CUNY J-School fail whale. Try browsing or do a search to continue on your way!</p>
+                <div class="fail-whale">
+                    <p>FAIL WHALE</p>
+                    <img src="<?php bloginfo('template_directory'); ?>/images/fail-whale" alt="Fail Whale"/>
+                </div>
+			
+			</div><!-- END .archive -->
+
+		</div><!-- END .content -->
+	
+		<?php get_sidebar(); ?>
+	
+		<div class="clear"></div>
+
+	</div><!-- END .container -->
+
+</div><!-- END .main -->
+
+<?php get_footer(); ?>
