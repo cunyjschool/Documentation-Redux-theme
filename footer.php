a17363c298faaad3fd5c88e866eee318c0c27092
joe <hover2pi@gmail.com>
2011-05-06 19:10:45 +0000

    Added a few basic CUNY links to the simplified footer.

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
 <div class="footer">
 	<div class="container">
 
-        Simple footer!
+        <div class="simple-footer">
+            
+            <span class="left">&copy; <?php echo date('Y'); ?> <a href="http://www.journalism.cuny.edu/">Graduate School of Journalism</a> | <a href="http://cuny.edu/">City University of New York</a></span>
+            <span class="right">219 W. 40th Street | New York, NY 10018 | <a href="http://www.journalism.cuny.edu/about/map-and-directions/">Map and Directions</a> | <a href="http://www.journalism.cuny.edu/about/campus-facilities/hours-of-operation/">Hours of Operation</a></span>
+        
+        </div>
 
 	</div><!-- END .container -->
 	<div class="clear"></div>
