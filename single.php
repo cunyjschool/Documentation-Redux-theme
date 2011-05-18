08fbbc845fb13b1aa62c8beef7c3837e3e093313
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-18 17:31:57 +0000

    More work on padding and styling

--- single.php
+++ single.php
@@ -12,6 +12,10 @@
 				
 					<h2><?php the_title() ?></h2>
 					
+					<div class="meta no-border">
+						<p>By <?php if ( function_exists( 'coauthors_posts_links' ) ) { coauthors_posts_links(); } else { the_author_posts_link(); } ?>&nbsp;&nbsp;&nbsp;Published <?php docredux_timestamp(); ?></p>
+					</div><!-- END .meta -->
+					
 					<div class="entry">
 						<?php the_content() ?>
 					</div>
@@ -19,8 +23,6 @@
 					<div class="clear"></div>
 					
 					<div class="meta">
-						<p>By <?php if ( function_exists( 'coauthors_posts_links' ) ) { coauthors_posts_links(); } else { the_author_posts_link(); } ?></p>
-						<p>Published at <?php the_time( 'g:i a l, M. jS, Y' ); ?></p>
 						<?php if ( $contexts = get_the_term_list( $post->ID, 'docredux_contexts', '', ', ', '' ) ) : ?>
 							<p>Contexts: <?php echo $contexts; ?></p>
 						<?php endif; ?>
