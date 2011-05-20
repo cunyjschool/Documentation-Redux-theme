bca2ddd0c119bed18df2a7b61896fd1ce0b0db14
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-20 22:22:14 +0000

    Tagging v0.3

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'DOCREDUX_VERSION', '0.3a' );
+define( 'DOCREDUX_VERSION', '0.3' );
 
 include_once( 'php/class.docredux_doc.php' );
 include_once( 'php/class.docredux_staff.php' );
