c616dbfbaa61172bb161499ee1ede1af4eff1836
joe <hover2pi@gmail.com>
2011-06-24 17:51:49 +0000

    Changed the services page to a general full-width page template. Updated stylesheet accordingly.

---
+++ page-no_sidebar.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: Page - No Sidebar
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="main">
+	
+	<div class="container">
+		
+		<div class="content full">
+			
+			<div class="entry">
+			
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				
+					<h2><?php the_title() ?></h2>
+					
+					<div class="meta">
+						<?php edit_post_link('Edit this post', '', ''); ?>
+					</div><!-- END .meta -->	
+			
+					<?php the_content() ?>
+							
+				<?php endwhile ; endif; ?>
+			
+			</div><!-- END .entry -->
+
+		</div><!-- END .content -->
+
+	</div><!-- END .container -->
+
+</div><!-- END .main -->
+
+<?php get_footer(); ?>
