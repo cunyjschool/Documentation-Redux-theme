7c03862acf20d0aa6f88f45d44ac32e020dbe954
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-07-11 21:09:05 +0000

    Add a column to the manage users view with number of documentation pieces

--- php/class.docredux_doc.php
+++ php/class.docredux_doc.php
@@ -16,7 +16,10 @@ class docredux_doc
 	function __construct() {
 		
 		// Add Database post type
-		$this->create_post_type();		
+		$this->create_post_type();
+		
+		add_filter( 'manage_users_columns', array( &$this, 'manage_user_columns' ) );
+		add_filter( 'manage_users_custom_column', array( &$this, 'handle_docredux_docs_num_column'), 10, 3 );
 		
 	} // END __construct()
 	
@@ -66,6 +69,35 @@ class docredux_doc
 		register_post_type( 'docredux_doc', $args );
 		
 	} // END - create_post_type()
+	
+	/**
+	 * Add a column to the manage users view for all of the documentation created by the user
+	 */
+	function manage_user_columns( $user_columns ) {
+		$user_columns[ __( '_docredux_docs_num' ) ] = __( 'Documentation' );
+		return $user_columns;
+	}
+	
+	/**
+	 * Show number of documentation for each user
+	 */
+	function handle_docredux_docs_num_column( $default, $column_name, $user_id ) {
+		if ( $column_name == __( '_docredux_docs_num' ) ) {
+			$args = array(
+				'posts_per_page' => '-1',
+				'post_type' => array(
+					'docredux_doc',
+				),
+				'author' => $user_id,
+			);
+			$total = new WP_Query( $args );
+			if ( $total->post_count > 0 )
+				return '<a href="' . get_admin_url( null, 'edit.php?post_type=docredux_doc&author=' . $user_id ) . '">' . $total->post_count . '</a>';
+			else
+				return $total->post_count;
+		}
+		return $default;
+	}
 	
 } // END class docredux_doc
 	
