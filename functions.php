21652a51da59122f303ab79f5c4dd8bec0c2a139
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-07-12 15:29:08 +0000

    Better pagination with previous and next links

--- functions.php
+++ functions.php
@@ -670,40 +670,26 @@ function docredux_timestamp( $post_id = null, $type = 'published' ) {
  * docredux_pagination()
  * Standardized pagination we can use anywhere in the loop
  */
-function docredux_pagination( $pages = '', $range = 2 ) {
-	global $wp_query, $paged;	
+function docredux_pagination() {
+	global $wp_query, $wp_rewrite;
+	$wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
+
+	$pagination = array(
+		'base' => @add_query_arg('page','%#%'),
+		'format' => '',
+		'total' => $wp_query->max_num_pages,
+		'current' => $current,
+		'type' => 'plain'
+		);
+
+	if( $wp_rewrite->using_permalinks() )
+		$pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
+
+	if( !empty($wp_query->query_vars['s']) )
+		$pagination['add_args'] = array( 's' => get_query_var( 's' ) );
+
+	echo "<div class='pagination paper'><span class='right'>Total results: " . $wp_query->found_posts . "</span>" . paginate_links( $pagination ) . '</div>';
 	
-	$showitems = ( $range * 2 ) + 1;  
-
-	if ( empty( $paged ) ) $paged = 1;
-
-	if ( '' == $pages ) {
-		$pages = $wp_query->max_num_pages;
-		if ( !$pages ) {
-			$pages = 1;
-		}
-	}	 
-
-	if ( 1 != $pages ) {
-		echo "<div class='pagination paper'><span class='right'>Total results: " . $wp_query->found_posts;
-		echo "</span>Pages:";
-		if ( $paged > 2 && $paged > $range+1 && $showitems < $pages )
-			echo "<a href='" . get_pagenum_link(1) . "'>&laquo;</a>";
-		if ( $paged > 1 && $showitems < $pages )
-			echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a>";
-
-		for ( $i=1; $i <= $pages; $i++ ) {
-			if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )) {
-				 echo ($paged == $i)? "<span class='current'>".$i."</span>":"<a href='".get_pagenum_link($i)."' class='inactive' >".$i."</a>";
-			}
-		}
-
-		if ( $paged < $pages && $showitems < $pages )
-			echo "<a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a>";  
-		if ( $paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages )
-			echo "<a href='".get_pagenum_link($pages)."'>&raquo;</a>";
-		echo "</div>\n";
-	 }
 } // END docredux_pagination()
 
 // Add custom taxonomies and custom post types counts to dashboard
