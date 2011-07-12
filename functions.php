34c2719768031c60564dbe2edf5784f3b4e11b8c
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-07-12 20:23:27 +0000

    Bump to v0.4b for testing on production

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'DOCREDUX_VERSION', '0.4a' );
+define( 'DOCREDUX_VERSION', '0.4b' );
 
 include_once( 'php/class.docredux_doc.php' );
 include_once( 'php/class.docredux_staff.php' );
