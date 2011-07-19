210af9526d7f911761103a009e9299238d7f99bd
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-07-19 20:39:18 +0000

    Clean up the styling when they're more than just a couple terms

--- sidebar-single_doc.php
+++ sidebar-single_doc.php
@@ -13,28 +13,28 @@
 		<h4><?php _e( 'About This Document' ); ?></h4>
 		
 		<div class="paper no-bottom-corners">
-			<p><label>Maintained:</label> <?php if ( function_exists( 'coauthors_posts_links' ) ) { coauthors_posts_links(); } else { the_author_posts_link(); } ?></p>
-			<p><label>Updated:</label> <?php docredux_timestamp( false, 'modified' ); ?></p>
+			<p><label class="float-left">Maintained:</label> <span><?php if ( function_exists( 'coauthors_posts_links' ) ) { coauthors_posts_links(); } else { the_author_posts_link(); } ?></span></p>
+			<p><label class="float-left">Updated:</label> <?php docredux_timestamp( false, 'modified' ); ?></p>
 		<?php if ( $contexts = get_the_term_list( $post->ID, 'docredux_contexts', '', ', ', '' ) ) : ?>
-			<p><label>Contexts:</label> <?php echo $contexts; ?></p>
+			<p><label class="float-left">Contexts:</label> <span><?php echo $contexts; ?></span></p>
 		<?php endif; ?>
 		<?php if ( $courses = get_the_term_list( $post->ID, 'docredux_courses', '', ', ', '' ) ) : ?>
-			<p><label>Courses:</label> <?php echo $courses; ?></p>
+			<p><label class="float-left">Courses:</label> <span><?php echo $courses; ?></span></p>
 		<?php endif; ?>
 		<?php if ( $topics = get_the_term_list( $post->ID, 'docredux_topics', '', ', ', '' ) ) : ?>
-			<p><label>Topics:</label> <?php echo $topics; ?></p>
+			<p><label class="float-left">Topics:</label> <span><?php echo $topics; ?></span></p>
 		<?php endif; ?>
 		<?php if ( $hardware = get_the_term_list( $post->ID, 'docredux_hardware', '', ', ', '' ) ) : ?>
-			<p><label>Hardware:</label> <?php echo $hardware; ?></p>
+			<p><label class="float-left">Hardware:</label> <span><?php echo $hardware; ?></span></p>
 		<?php endif; ?>
 		<?php if ( $software = get_the_term_list( $post->ID, 'docredux_software', '', ', ', '' ) ) : ?>
-			<p><label>Software:</label> <?php echo $software; ?></p>
+			<p><label class="float-left">Software:</label> <span><?php echo $software; ?></span></p>
 		<?php endif; ?>
 		<?php if ( $wpthemes = get_the_term_list( $post->ID, 'docredux_wpthemes', '', ', ', '' ) ) : ?>
-			<p><label>WordPress themes:</label> <?php echo $wpthemes; ?></p>
+			<p><label class="float-left">WordPress themes:</label> <span><?php echo $wpthemes; ?></span></p>
 		<?php endif; ?>
 		<?php if ( $wpplugins = get_the_term_list( $post->ID, 'docredux_wpplugins', '', ', ', '' ) ) : ?>
-			<p><label>WordPress plugins:</label> <?php echo $wpplugins; ?></p>
+			<p><label class="float-left">WordPress plugins:</label> <span><?php echo $wpplugins; ?></span></p>
 		<?php endif; ?>
 		</div><!-- END .no-bottom-corners -->
 	</div><!-- END .widget -->
